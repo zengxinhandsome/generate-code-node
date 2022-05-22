ac21244d37e67699bde4f8a675b08ce12111ef9a
DaWenXi <zengxin@bianfeng.com>
2022-05-22 19:47:51 +0800

    feat: add script

---
+++ createTemplate.js
@@ -0,0 +1,29 @@
+import fs from 'fs';
+import path from 'path';
+
+import fileJsonData from './fileJson.js';
+
+/**
+ * 通过 json data 生成模板文件
+ */
+const createFolderByData = (data, dir) => {
+  data.forEach(item => {
+    const currentDir = path.join(dir, item.name);
+    if (item.type === 'folder') {
+      if (!fs.existsSync(currentDir)) {
+        fs.mkdirSync(currentDir);
+      }
+    } else if (item.type === 'file') {
+      if (!fs.existsSync(currentDir)) {
+        fs.writeFileSync(currentDir, item.content, err => {});
+      }
+    }
+
+    if (item.children) {
+      createFolderByData(item.children, currentDir);
+    }
+  });
+};
+
+createFolderByData(fileJsonData, './');
+
--- index.js
+++ index.js
@@ -1,31 +1,5 @@
 import fs from 'fs';
 import path from 'path';
-import fileJsonData from './fileJson.js';
-
-/**
- * 通过 json data 生成模板文件 
- */
-const createFolderByData = (data, dir) => {
-  data.forEach(item => {
-    const currentDir = path.join(dir, item.name);
-    if (item.type === 'folder') {
-      if (!fs.existsSync(currentDir)) {
-        fs.mkdirSync(currentDir);
-      }
-    } else if (item.type === 'file') {
-      if (!fs.existsSync(currentDir)) {
-        fs.writeFileSync(currentDir, item.content, err => {});
-      }
-    }
-
-    if (item.children) {
-      createFolderByData(item.children, currentDir);
-    }
-  });
-};
-
-createFolderByData(fileJsonData, './');
-
 
 /**
  * 通过模板文件生成对应的 json data
